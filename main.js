01c71883a40179f4b04cf4d9b316896752940c43
Osita <chrisjerry070@gmail.com>
2024-01-15 16:12:03 +0400

    ajusted width and height

--- main.js
+++ main.js
@@ -1,18 +1,33 @@
-
-let btn = document.getElementById("bttn");
-
-
- function check() {
-    let entered = document.getElementById("request").value;
+function check() {
     document.getElementById("feedback").style.color = "light-orange";
     document.getElementById("feedback").style.fontSize = "25px";
+    let entered = document.getElementById("request").value;
+    
     let splitString = entered.split("");
     let reverseArray = splitString.reverse();
     let joinArray = reverseArray.join("");
+
     if (entered == joinArray) {
         document.getElementById("feedback").innerText = (`${entered} is a palindrome`)
     } else {
         document.getElementById("feedback").innerText = (`${entered} is not a palindrome`)
-    }
-    console.log(joinArray);
+    };
+
+    let myArr = [];
+    let obj = {};
+    obj.task = entered;
+    myArr.push(obj);
+    console.log(obj);
+    console.log(myArr);
+
+    var fs = require('fs');
+    fs.writeFile("data.json", myArr, err=> {
+        if (err) {
+            console.log('ERROR' ,err)
+        } else {
+            console.log('json data is wriiten successfully')
+        }
+    });
+
 };
+
